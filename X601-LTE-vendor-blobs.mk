c54caa5a0c97bbd8a850a4997687fe234e595629
zac6ix <nanaquameallday@gmail.com>
2017-10-14 22:12:16 +0000

    more, fx

---
+++ X601-LTE-vendor-blobs.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X6O1-LTE/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X6O1-LTE/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X6O1-LTE/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X6O1-LTE/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X6O1-LTE/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X6O1-LTE/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X6O1-LTE/vendor,system/vendor)
